0e89c8629fe3217c5f09ebec852f50ccbe995f63
Christian Zintel <80048997+czintel@users.noreply.github.com>
2023-05-08 18:30:44 +0200

    added calculation of winner

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ export default function Board() {
 	const [xIsNext, setXIsNext] = useState(true);
 
 	function handleClick(i) {
+		if (squares[i] || calculateWinner(squares)) {
+			return;
+		}
+
 		const nextSquares = squares.slice();
 
 		if (xIsNext) {
@@ -43,4 +47,26 @@ export default function Board() {
 			</div>
 		</React.Fragment>
 	);
+}
+
+function calculateWinner(squares) {
+	const lines = [
+		[0, 1, 2],
+		[3, 4, 5],
+		[6, 7, 8],
+		[0, 3, 6],
+		[1, 4, 7],
+		[2, 5, 8],
+		[0, 4, 8],
+		[2, 4, 6],
+	];
+
+	for (let i = 0; i < lines.length; i++) {
+		const [a, b, c] = lines[i];
+
+		if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+			return squares[a];
+		}
+	}
+	return null;
 }
